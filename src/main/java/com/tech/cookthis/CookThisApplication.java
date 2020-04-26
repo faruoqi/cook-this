ce18daad887211e11908affe67e9faa3fabaff80
faruoqi <rezafaruoqi@gmail.com>
2020-04-26 13:43:53 +0700

    initial commit

---
+++ src/main/java/com/tech/cookthis/CookThisApplication.java
@@ -0,0 +1,13 @@
+package com.tech.cookthis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookThisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookThisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tech/cookthis/CookThisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.cookthis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookThisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
